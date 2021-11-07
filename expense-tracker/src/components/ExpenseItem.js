4ae0a17b235791d144e873860dac168afb43359e
ishitarastogi <ishita3099@gmail.com>
2021-11-07 19:58:47 +0530

    expense-item component

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,15 +1,21 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate=new Date(2021,11,7 );
+    const expenseTitle='car Insurance'
+    const expenseAmount=295;
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
+
+//can't render<div>{expenseDate}</div> as it is a date object 
+//so need to convert it in string {expenseDate.toISOString()}
